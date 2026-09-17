1c5aae3b1d8c6626f47d86855a1f61ab32a63295
Timur <tima.filippov123@mail.ru>
2026-09-18 01:37:17 +0300

    Update message in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-int main()
-{
-std::cout << "Hello, my name is Timur. I am from IU5-12/4!" << std::endl;
+int main() {
+std::cout << "Updated message!" << std::endl;
 return 0;
 }
